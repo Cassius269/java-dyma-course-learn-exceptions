049846123a4b46de49614af58f6986f0f0a5f210
Cassius269 <fahamygaston@gmail.com>
2025-11-06 21:25:57 +0100

    attraper des exceptions natives

--- src/main/java/com/fahami/cda/Main.java
+++ src/main/java/com/fahami/cda/Main.java
@@ -3,5 +3,18 @@ package com.fahami.cda;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        String name = "JOHN";
+        try {
+            int number = Integer.parseInt(name);
+            IO.println(number);
+        }catch(NumberFormatException e){
+            // e.printStackTrace();
+            IO.println("Exception levée : " + e.getMessage());
+        }catch(Exception e){
+            IO.println("Exception générique : " + e.getMessage());
+        }
+
+        IO.println("Tout s'est bien executé");
     }
 }
